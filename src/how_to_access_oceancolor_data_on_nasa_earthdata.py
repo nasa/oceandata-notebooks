95f1512b6d5bb74b7cd6f3a02aabeeb9bebb84c9
Ian Carroll <ian.t.carroll@nasa.gov>
2024-03-08 12:08:01 +0000

    new how-to

---
+++ src/how_to_access_oceancolor_data_on_nasa_earthdata.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.0
+#   kernelspec:
+#     display_name: Python 3.10 (conda)
+#     language: python
+#     name: python3
+# ---
+
+# + [markdown] id="thB-pNE2YUT7"
+# # Accessing OB DAAC Ocean Color Data With Python
+
+# + [markdown] id="EMTS0oIRY3dj"
+# ### Overview
+# This article demonstrates accessing and analyzing ocean color data using Python from the NASA OB DAAC archives. Currently, there are several ways to find and access ocean color data:
+# 1. [NASA's Earthdata Search](https://search.earthdata.nasa.gov/search)
+# 2. [NASA's CMR API](https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html)
+# 3. [OB.DAAC OPENDAP](https://oceandata.sci.gsfc.nasa.gov/opendap/)
+# 4. [Ocean color file search](https://oceandata.sci.gsfc.nasa.gov/api/file_search_help)
+
+# + [markdown] id="9vivpFtxIt1O"
+# MODIS Aqua, L2 - Ches Bay
+# L3 - chlorophyll a map
+# -
+
+
