80c0003e54eb636add954f318ebdd2e101e9153b
Anna Windle <awindle110@gmail.com>
2024-04-16 13:39:32 +0000

    changed to PACE data

--- src/1_PACE_data_access.py
+++ src/1_PACE_data_access.py
@@ -42,16 +42,20 @@
 # <hr>
 
 # # 1. Accessing PACE data via the `earthaccess` library
-
+#
+# ## Learning objectives
+#
+# At the end of this notebook you will know:
+#
+# * How to authenticate with `earthaccess`
+# * How to use `earthaccess` to search for PACE data using search filters
+# * How to download PACE data
+#
+# ### Outline
 # In this example we will use the `earthaccess` library to search for data collections from NASA Earthdata. `earthaccess` is a Python library that simplifies data discovery and access to NASA Earth science data by providing an abstraction layer for NASA’s [Common Metadata Repository (CMR) API](https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html) Search API. The library makes searching for data more approachable by using a simpler notation instead of low level HTTP queries. `earthaccess` takes the trouble out of Earthdata Login **authentication**, makes **search** easier, and provides a stream-line way to download or stream search results into an `xarray` object.
 #
 # For more on `earthaccess` visit the [`earthaccess` GitHub](https://github.com/nsidc/earthaccess) page and/or the [`earthaccess` documentation](https://earthaccess.readthedocs.io/en/latest/) site. Be aware that `earthaccess` is under active development. 
 #
-# ## Learning objectives
-# 1. How to authenticate with `earthaccess`
-# 2. How to use `earthaccess` to search for PACE data using search filters
-# 3. How to download PACE data
-
 # <div class="alert alert-info" role="alert">
 #
 # ## <a id='TOC_TOP'>Contents
@@ -66,7 +70,11 @@
 
 # We begin by importing all of the libraries that we need to run this notebook. If you have built your python using the environment file provided in this repository, then you should have everything you need. For more information on building environment, please see the repository README.
 
-import earthaccess 
+# + active=""
+# TODO: make an environment.yml file
+# -
+
+import earthaccess
 
 # <div class="alert alert-info" role="alert">
 #
@@ -75,7 +83,7 @@ import earthaccess
 #
 # </div>
 
-# We will start by authenticating using our Earthdata Login credentials. Authentication is not necessarily needed to search for publicaly available data collections in Earthdata, but is always need to download or access data from the NASA Earthdata archives. We can use `login` method from the `earthaccess` library here. This will create a authenticated session using our Earthdata Login credential. Our credentials can be passed along via **environmental variables** or by a **.netrc** file save in the home/user profile directory. If your credentials are not available in either location, we will be prompt to input our credentials and a **.netrc** will be created and saved for us.  
+# Next, we will start by authenticating using our Earthdata Login credentials. Authentication is not necessarily needed to search for publicaly available data collections in Earthdata, but is always need to download or access data from the NASA Earthdata archives. We can use `login` method from the `earthaccess` library here. This will create a authenticated session using our Earthdata Login credential. Our credentials can be passed along via **environmental variables** or by a **.netrc** file save in the home/user profile directory. If your credentials are not available in either location, we will be prompt to input our credentials and a **.netrc** will be created and saved for us.  
 
 auth = earthaccess.login()
 # are we authenticated?
@@ -95,39 +103,42 @@ if not auth.authenticated:
 
 # | Shortname | Collection Concept ID | DOI |
 # | --- | --- | --- |
-# | MODISA_L2_OC | C2330511440-OB_DAAC | 10.5067/AQUA/MODIS/L2/OC/2022 |
+# | PACE_OCI_L2_AOP_NRT | C2330511440-OB_DAAC | XXXX |
 #
-#
+
+# + active=""
+# TODO: Figure out concept ID and DOI for L2 pace files- maybe include some description on how to find this?
+# -
 
 results = earthaccess.search_data(
-    short_name = "MODISA_L2_OC",
+    short_name = "PACE_OCI_L2_AOP_NRT",
     cloud_hosted = True,
     count = 10    # Restricting to 10 records returned
 )
 
 results = earthaccess.search_data(
-    concept_id = "C2330511440-OB_DAAC",
+    concept_id = "C2910373786-OB_CLOUD",
     cloud_hosted = True,
     count = 10    # Restricting to 10 records returned
 )
 
 results = earthaccess.search_data(
-    doi = "10.5067/AQUA/MODIS/L2/OC/2022",
+    doi = "",
     cloud_hosted = True,
     count = 10    # Restricting to 10 records returned
 )
 
-# We can refine our search by passing more parameters that describe the spatiotemporal domain of our use case. Here, we use the `temporal` parameter to request a date range and the `bounding_box` parameter to request granules that intersect with a bounding box.  
+# We can refine our search by passing more parameters that describe the spatiotemporal domain of our use case. Here, we use the `temporal` parameter to request a date range and the `bounding_box` parameter to request granules that intersect with a bounding box. We can even provide a `cloud_cover` threshold to limit files that have a lower percetnage of cloud cover
 
-date_range = ("2022-11-19", "2023-04-06")
+date_range = ("2024-04-01", "2024-04-12")
 bbox = (-76.75,36.97,-75.74,39.01)
 
 results = earthaccess.search_data(
-    short_name = "MODISA_L12_OC",
+    short_name = "PACE_OCI_L2_AOP_NRT",
     cloud_hosted = True,
     temporal = date_range,
-    bounding_box = bbox,
-    cloud_cover = (0,50)
+    bounding_box = bbox, 
+    cloud_cover = (0,100)
 )
 
 # <div class="alert alert-info" role="alert">
@@ -150,7 +161,7 @@ results[0]
 
 downloaded_files = earthaccess.download(
     results[0:2],
-    local_path='Desktop/learn-pace/data/',
+    local_path='storage/',
 )
 
 # If you have completed this section, you can now continue to work through the rest of the notebooks in the repository and learn more about working with PACE data. 
@@ -158,7 +169,7 @@ downloaded_files = earthaccess.download(
 # <hr>
 # <a href="../Index.ipynb"><< Index</a>
 # <br>
-# <a href="./1_2_PACE_file_structure.ipynb">Understanding PACE file structure >></a>
+# <a href="./2_PACE_file_structure.ipynb">Understanding PACE file structure >></a>
 # <hr>
 #
 # <a href="https://oceancolor.gsfc.nasa.gov/" target="_blank">NASA Ocean Color website</a> | <a href="https://oceancolor.gsfc.nasa.gov/resources/docs/tutorials/" target="_blank">NASA Ocean Color Tutorials</a></span></p>
--- src/2_PACE_file_structure.py
+++ src/2_PACE_file_structure.py
@@ -7,9 +7,9 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.16.0
 #   kernelspec:
-#     display_name: Python 3.10 (conda)
+#     display_name: custom
 #     language: python
-#     name: python3
+#     name: custom
 # ---
 
 # <table><tr>
@@ -17,9 +17,11 @@
 #
 # <td> <img src="https://oceancolor.gsfc.nasa.gov/images/ob-logo-svg-2.svg" alt="Drawing" align='right', style="width: 240px;"/> </td>
 #
-# <td> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/2449px-NASA_logo.svg.png" align='right', alt="Drawing" style="width: 70px;"/> </td>
+# <td> <img src="https://www.nasa.gov/wp-content/uploads/2023/04/nasa-logo-web-rgb.png" align='right', alt="Drawing" style="width: 70px;"/> </td>
 #
 # </tr></table>
+#
+#
 
 # <a href="../Index.ipynb"><< Index</a>
 # <br>
@@ -42,25 +44,30 @@
 
 # # 2. Understanding PACE file structure
 
-# In this example we will use the 'earthaccess' library to access a Level-1B, Level-2, and Level-3 PACE files to understand the file structure 
-#
 # ## Learning objectives
-# 1. What a netCDF format is
-# 2. Brief introduction to the `xarray` library
-# 3. What variables are present in each group for PACE data files (L1B, L2, and L3)
-
+#
+# At the end of this notebok you will know:
+# * What a netCDF format is
+# * How to use `xarray` library to open PACE data
+# * What variables are present in each group for PACE data files (L1B, L2, and L3)
+#
+# ## Outline
+# In this example we will use the 'earthaccess' library to access a Level-1B, Level-2, and Level-3 PACE netcdf files and open them using `xarray`.
+#
+#
+# **`netCDF`** (network Common Data Form) is a file format for storing multidimensional scientific data (variables). It is optimized for array-oriented data access and support a machine-independent format for representing scientific data. Files ending in `.nc` are netCDF files.
+#
+# **<a href="https://xarray.dev/" target="_blank">`xarray`</a>** is a library that supports the use of multi-dimensional arrays in Python. It is widely used to handle Earth observation data, which often involves multiple dimensions — for instance, longitude, latitude, time, and channels/bands. <br>
+#
 # <div class="alert alert-info" role="alert">
 #
 # ## <a id='TOC_TOP'>Contents
 #
 # </div>
 #     
-#  1. [Intro to xarray](#section1)
-#  2. [Querying PACE L1B file structure](#section2)
-#  3. [Querying PACE L2 file structure](#section3)
-#  4. [Querying PACE L3 file structure](#section4)
-#  5. 
-#     
+#  1. [Querying PACE L1B file structure](#section2)
+#  2. [Querying PACE L2 file structure](#section3)
+#  3. [Querying PACE L3 file structure](#section4)
 #
 # <hr>
 
@@ -69,6 +76,8 @@
 import earthaccess
 import xarray as xr
 import netCDF4 as nc
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 # ## `earthaccess` authentication
 
@@ -80,54 +89,154 @@ if not auth.authenticated:
 
 # <div class="alert alert-info" role="alert">
 #
-# ## <a id='section1'>1. Intro to xarray
+# ## <a id='section1'>1. Querying PACE L1B file structure
 # [Back to top](#TOC_TOP)
 #
 # </div>
 
-# **<a href="https://xarray.dev/" target="_blank">`xarray`</a>** is a library that supports the use of multi-dimensional arrays in Python. It is widely used to handle Earth observation data, which often involves multiple dimensions — for instance, longitude, latitude, time, and channels/bands. <br>
-#
-# Let's open up a PACE L1B, L2, and L3 file from the same scene using `earthaccess`:
+# Let's use `xarray` to open up a PACE L1B netCDF files of the same scene using `earthaccess`. We will use the same search method used in <a href="./2_PACE_data_access.ipynb">2_PACE_data_access.ipynb </a> to access the data:
 
 # +
-date_range = ("2024-03-01", "2024-03-03")
+date_range = ("2024-04-01", "2024-04-16")
 bbox = (-76.75,36.97,-75.74,39.01)
 
 results = earthaccess.search_data(
-    short_name = "MODISA_L1",
+    short_name = "PACE_OCI_L1B_SCI",
+    cloud_hosted = True,
+    temporal = date_range,
+    bounding_box = bbox,
+    #cloud_cover = (0,100) #TODO: figure out why cloud_cover isn't working
+)
+
+L1B_files = earthaccess.open(results)
+L1B_files
+# -
+
+# Let's open the first file of the L1B_files list:
+
+dfs = xr.open_dataset(L1B_files[1])
+dfs
+
+# Notice that the xarray.Dataset has missing data in some of the categories. `xarray` cannot be used to open multi-group hierarchies or to list groups within a netCDF file, but it can open a specific group if you know it’s path.
+#
+# Tip: The `datatree` python library can be used to show group names. Uncomment below to run:
+
+# +
+#from datatree import open_datatree
+
+#datatree = open_datatree(L1B_files[0], engine='h5netcdf') 
+#datatree
+# -
+
+# The PACE L1B dataset groups are `sensor_band_parameters`, `scan_line_attributes`, `geolocation_data`, `navigation_data`, and `observation_data`. Let's open the `observation_data` group:
+
+df_obs = xr.open_dataset(L1B_files[1], group='observation_data')
+df_obs
+
+# Now you can view the Dimensions, Coordinates, and Variables of this group. To show/hide attributes, press the paper icon on the right hand side of each variable. To show/hide data reprensetaton, press the cylinder icon. 
+
+# The dimensions of the `rhot_blue` variable is (blue_bands, number_of_scans, ccd_pixels) and has the shape (119, 1709, 1272). 
+
+df_obs['rhot_blue'].shape
+
+# Let's quickly plot one blue band:
+
+df_obs['rhot_blue'][100,:,:].plot()
+
+# <div class="alert alert-info" role="alert">
+#
+# ## <a id='section1'>2. Querying PACE L2 file structure
+# [Back to top](#TOC_TOP)
+#
+# </div>
+
+# Now, let's open a PACE L2 Apparent Optical Data (AOP) file. We'll use the same `earthaccess` search to find the data:
+
+# +
+date_range = ("2024-04-01", "2024-04-16")
+bbox = (-76.75,36.97,-75.74,39.01)
+
+results = earthaccess.search_data(
+    short_name = "PACE_OCI_L2_AOP_NRT",
     cloud_hosted = True,
     temporal = date_range,
     bounding_box = bbox
 )
-L1B_file = results[0]
-L1B_file
-#earthaccess.open(results[0])
+
+L2_files = earthaccess.open(results)
+L2_files
+
+# +
+#from datatree import open_datatree
+
+#datatree = open_datatree(L2_files[0], engine='h5netcdf') 
+#datatree
 # -
 
-df = xr.open_dataset(earthaccess.open(results))
-df
-
-# Notice that the xarray.Dataset has missing data in some of the categories. `xarray` cannot be used to open multi-group hierarchies or to list groups within a netCDF file, but it can open a specific group if you know it’s path.
-#
-#
-# Tip: The `netCDF4` library can access groups. You can use this function to view the groups that you want to specify with `xarray`
-#
-
-df_nc = nc.Dataset(file, "r")
-df_nc.groups.keys()
-
-# A PACE L1B file has 5 groups: 'sensor_band_parameters', 'scan_line_attributes', 'geophysical_data', 'navigation_data', 'processing_control.
-#
-# Now we can open each group in `xarray`:
-
-df_bands = xr.open_dataset(file, group='sensor_band_parameters')
-df_bands
-
-# Now you can view the Dimensions, Coordinates, and Variables of this group. To show/hide attributes, press the paper icon on the right hand side of each variable. To show/hide data reprensetaton, press the cylinder icon. 
-
+# The PACE L2 dataset groups are `sensor_band_parameters`, `scan_line_attributes`, `geophysical_data`, `navigation_data`. <br>
 # Let's look at the geophysical_data group
 
-df_geo = xr.open_dataset(file, group='geophysical_data')
+df_geo = xr.open_dataset(L2_files[1], group='geophysical_data')
 df_geo
+
+# The Rrs data variable has a shape of (1710, 1272, 184). Let's map a random Rrs wavelength:
+
+df_geo['Rrs'][:,:,100].plot()
+
+# Right now, the scene is being plotted using number_of_lines and pixels_per_line (x,y). Let's add some lat and lon values to map it in a real coordinate space. To do this, we need to create a new xarrray dataset (rrs_xds) and pull in information from the `navigational_data` group (df_nav).
+
+df_nav = xr.open_dataset(L2_files[1], group='navigation_data')
+df_nav
+
+rrs_xds = df_nav.rename({"pixel_control_points": "pixels_per_line"})
+rrs_xds = xr.merge((df_geo, rrs_xds))
+rrs_xds = rrs_xds.set_coords(("longitude", "latitude"))
+rrs_xds
+
+# Although we now have coordinates, they won't help much because the data are not "gridded" by latitude and longitude.
+# The Level 2 data are the original instument swath, and have not been resampled to a regular grid. Therefore latitude
+# and longitude are known, but cannot be used to "look-up" values like you can along the dataset's dimensions. <br>
+#
+# Let's plot a scatter plot of the pixel locations so we can see the irregular spacing. 
+
+rrs_xds.sel({"number_of_lines": slice(None, None, 1720//20),
+        "pixels_per_line": slice(None, None, 1272//20),}).plot.scatter(x="longitude", y="latitude")
+
+# Let's plot this new xarray dataset the same way as before, but add lat, lon
+
+rrs_xds.Rrs[:,:,100].plot(x='longitude', y='latitude', cmap='viridis', vmin=0)
+
+
+# If you wanna get fancy, add a coastline
+
+# +
+fig = plt.figure()
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines()
+ax.gridlines(draw_labels={"left": "y", "bottom": "x"})
+
+rrs_xds.Rrs[:,:,100].plot(x='longitude', y='latitude', cmap='viridis', vmin=0)
+
+# +
+ax.plot(-60, 43, marker='o', color='red')
+
+fig
+# -
+
+rrs_xds.Rrs
+
+rrs_xds_point = rrs_xds.sel({
+    "number_of_lines": 1650, 
+    "pixels_per_line": 1270,
+})
+rrs_xds_point.coords
+
+rrs_xds_point["Rrs"].plot.line()
+
+# # Combining data 
+
+dfs = xr.open_mfdataset(L1B_files, group="observation_data", combine="nested", 
+                        concat_dim="number_of_scans", engine="h5netcdf")
+dfs
 
 
