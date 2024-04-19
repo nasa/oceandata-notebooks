7682ae9c17c523764c8a9b5f6acc7151a87dac12
Ian Carroll <ian.t.carroll@nasa.gov>
2024-04-19 15:59:47 +0000

    add currently published notebooks

--- src/how_to_access_oceancolor_data_on_nasa_earthdata.py
+++ src/how_to_access_oceancolor_data_on_nasa_earthdata.py
@@ -5,9 +5,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.16.0
+#       jupytext_version: 1.16.1
 #   kernelspec:
-#     display_name: Python 3.10 (conda)
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
