02e3844581fa619a838b9033231a088437bf007e
Sean Foley <seanremy@gmail.com>
2024-05-15 16:11:35 -0400

    add some plots from Meng, make TO DO list for remaining tasks

--- src/4_MAP_visualizations.py
+++ src/4_MAP_visualizations.py
@@ -12,6 +12,13 @@
 #     name: python3
 # ---
 
+# TO DO LIST
+# - [ ] outline
+# - [ ] table of contents
+# - [ ] SPEXOne stuff
+# - [ ] add more inline comments to all code blocks
+# - [ ] linting?
+
 # <table><tr>
 #
 #
@@ -22,14 +29,12 @@
 # </tr></table>
 
 # TO DO: update this once there are filenames for the other notebooks
-# <a href="../Index.ipynb"><< Index</a>
-# <br>
-# <a href="./1_2_OLCI_file_structure.ipynb">Understanding PACE file structure >></a>
+# <a href="../Index.ipynb"><< PLACEHOLDER</a> | <a href="../Index.ipynb">PLACEHOLDER >></a>
 
 # <font color="dodgerblue">**Ocean Biology Processing Group**</font> <br>
 # **Copyright:** 2024 NASA OBPG <br>
 # **License:** MIT <br>
-# **Authors:** Sean Foley (NASA/MSU)
+# **Authors:** Sean Foley (NASA/MSU), Meng Gao (NASA/SSAI)
 
 # <div class="alert alert-block alert-warning">
 #     
@@ -71,6 +76,8 @@
 # +
 import os
 from pathlib import Path
+import shutil
+import warnings
 
 # TO DO: final check on dependencies, update environment.yml
 from apng import APNG
@@ -81,8 +88,8 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 from netCDF4 import Dataset
 import numpy as np
-import shutil
 from tqdm.notebook import tqdm
+import xarray as xr
 # -
 
 # Next, we will download some HARP2 Level 1C data. Level 1C corresponds to geolocated imagery. This means the imagery coming from the satellite has been calibrated and assigned to locations on the Earth's surface. Note that this might take a while, depending on the speed of your internet connection, and the progress bar will seem frozen because we're only downloading one file.
@@ -105,7 +112,9 @@ downloaded_files = earthaccess.download(
     results[0],
     local_path='storage/',
 )
-harp_data = Dataset(Path(downloaded_files[0]))
+# harp_nc = Dataset(Path(downloaded_files[0]))
+groups = [None, 'sensor_views_bands', 'bin_attributes', 'geolocation_data', 'observation_data']
+harp_data = xr.merge([xr.open_dataset(downloaded_files[0], group=g) for g in groups])
 # -
 
 # ## HARP2 L1C: Understanding the Data
@@ -114,8 +123,8 @@ harp_data = Dataset(Path(downloaded_files[0]))
 
 # +
 # TO DO: make these plots prettier and colorblind friendly
-angles = harp_data["sensor_views_bands/sensor_view_angle"]
-wavelengths = harp_data["sensor_views_bands/intensity_wavelength"]
+angles = harp_data["sensor_view_angle"]
+wavelengths = harp_data["intensity_wavelength"]
 arange = np.arange(angles.shape[0])
 
 fig, axs = plt.subplots(2, 1, figsize=(16, 7))
@@ -141,15 +150,15 @@ axs[1].legend();
 # +
 # get the I, Q, and U components of the Stokes vector out of the netCDF data.
 # The [..., 0] gets rid of the extra axis at the end. You can instead use [:] to pull out the array in its original shape.
-i_stokes = harp_data["observation_data/i"][..., 0]
-q_stokes = harp_data["observation_data/q"][..., 0]
-u_stokes = harp_data["observation_data/u"][..., 0]
+i_stokes = harp_data["i"][..., 0].to_masked_array()
+q_stokes = harp_data["q"][..., 0].to_masked_array()
+u_stokes = harp_data["u"][..., 0].to_masked_array()
 
 # The first 10 channels are green, the next 60 channels are red, and the final 10 channels are blue (we're skipping NIR)
 # In each of those groups of channels, we get the index of the minimum absolute value of the camera angle, corresponding to our nadir view
-green_nadir_idx = np.argmin(np.abs(angles[:10]))
-red_nadir_idx = 10 + np.argmin(np.abs(angles[10:70]))
-blue_nadir_idx = 80 + np.argmin(np.abs(angles[80:]))
+green_nadir_idx = np.argmin(np.abs(angles[:10].values))
+red_nadir_idx = 10 + np.argmin(np.abs(angles[10:70].values))
+blue_nadir_idx = 80 + np.argmin(np.abs(angles[80:].values))
 
 # Then, get the data at the nadir indices.
 rgb_i, rgb_q, rgb_u = [arr[:, :, (red_nadir_idx, green_nadir_idx, blue_nadir_idx)] for arr in [i_stokes, q_stokes, u_stokes]]
@@ -158,8 +167,8 @@ rgb_i, rgb_q, rgb_u = [(arr - arr.min()) / (arr.max() - arr.min()) for arr in [r
 rgb_i, rgb_q, rgb_u = [arr ** (3/4) for arr in [rgb_i, rgb_q, rgb_u]]  # bring out some of the darker colors
 
 # Get latitude and longitude to use for the map projection.
-lat = harp_data['geolocation_data/latitude'][:]
-lon = harp_data['geolocation_data/longitude'][:]
+lat = harp_data['latitude'].values
+lon = harp_data['longitude'].values
 
 # Get a mask of valid latitudes / longitudes to crop the plots later.
 valid_lon = lon[~(rgb_i.mask.all(axis=-1))]
@@ -176,8 +185,11 @@ for i, (arr, title) in enumerate([(rgb_i, 'I'), (rgb_q, 'Q'), (rgb_u, 'U')]):
                           xlocs=mticker.FixedLocator(np.arange(-180,180.1,10)),
                           ylocs=mticker.FixedLocator(np.arange(-90,90.1,10)))
     gl.top_labels, gl.right_labels = False, False
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="invalid value encountered in cast", category=RuntimeWarning, module='numpy')
+        arr = (255 * arr).astype(np.uint8)
     # This line actually plots the data, but you have to convert the arrays to unsigned 8-bit integers first (0-255).
-    axs[i].pcolormesh(lon, lat, (255 * arr).astype(np.uint8), transform=projection);
+    axs[i].pcolormesh(lon, lat, arr, transform=projection);
     axs[i].coastlines(color='grey')  # draw coastlines
     # Crop the data using our valid latitude/longitude from earlier, with 1 degree of padding
     axs[i].set_xlim(max(-180, valid_lon.min() - 1), min(180, valid_lon.max() + 1))
@@ -191,10 +203,13 @@ fig.suptitle(f"{harp_data.product_name} RGB");
 #
 # [This Wikipedia plot](https://upload.wikimedia.org/wikipedia/commons/3/31/StokesParameters.png) is very helpful for understanding what exactly the Q and U components of the Stokes vector mean, exactly. Basically, Q describes how much the light is oriented in -90&deg;/90&deg; vs. 0&deg;/180&deg;, while U describes how it's oriented in -135&deg;/45&deg; vs. -45&deg;/135&deg;.
 #
-# Next, let's take a look at the degree of linear polarization (DoLP). TO DO: pick a better scene and explain it
+# Next, let's take a look at the degree of linear polarization (DoLP).
+#
+#
+# TO DO: pick a better scene and explain it
 
 # +
-dolp = harp_data["observation_data/dolp"][..., 0]
+dolp = harp_data["dolp"][..., 0].to_masked_array()
 rgb_dolp = dolp[:, :, (red_nadir_idx, green_nadir_idx, blue_nadir_idx)]
 
 # Creates a figure with 1 row and 2 columns, a fitting figsize for many screens, and uses the projection defined above.
@@ -208,33 +223,100 @@ for i, (arr, title) in enumerate([(rgb_i, 'I'), (rgb_dolp, 'DoLP')]):
                           ylocs=mticker.FixedLocator(np.arange(-90,90.1,10)))
     gl.top_labels, gl.right_labels = False, False
     # This line actually plots the data, but you have to convert the arrays to unsigned 8-bit integers first (0-255).
-    axs[i].pcolormesh(lon, lat, (255 * arr).astype(np.uint8), transform=projection);
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="invalid value encountered in cast", category=RuntimeWarning, module='numpy')
+        arr = (255 * arr).astype(np.uint8)
+    axs[i].pcolormesh(lon, lat, arr, transform=projection);
     axs[i].coastlines(color='grey')  # draw coastlines
     # Crop the data using our valid latitude/longitude from earlier, with 1 degree of padding
     axs[i].set_xlim(max(-180, valid_lon.min() - 1), min(180, valid_lon.max() + 1))
     axs[i].set_ylim(max(-90, valid_lat.min() - 1), min(90, valid_lat.max() + 1))
     axs[i].set_title(title)
+
+fig, ax = plt.subplots(figsize=(16, 6))
+wv_uq = np.unique(wavelengths.values[..., 0])
+colors = ['b', 'g', 'r', 'black']
+for wv_idx in range(4):
+    wv = wv_uq[wv_idx]
+    wv_mask = wavelengths.values[..., 0] == wv
+    dolp_mean = np.reshape(dolp, (-1, dolp.shape[-1])).mean(axis=0)
+    ax.plot(angles.values[wv_mask], dolp_mean[wv_mask], '.-', label=str(wv), color=colors[wv_idx])
+ax.legend()
+ax.set_xlabel("Nominal view angle ($\\circ$)")
+ax.set_ylabel("DoLP")
+ax.set_title("Mean DoLP by view angle");
 # -
 
-# TO DO: make a plot for an entire orbit or entire day of data
+# ## Radiance to Reflectance
+#
+# We can convert radiance into reflectance. TO DO
+
+# +
+f0 = harp_data['intensity_f0'].to_masked_array()[:, 0]
+sza = harp_data['solar_zenith_angle'].to_masked_array()
+r_sun_earth = np.float32(harp_data.sun_earth_distance)
+
+def rad_to_refl(rad, f0, sza, r):
+    """Convert radiance to reflectance.
+    Args:
+        rad: Radiance.
+        f0: Solar irradiance.
+        sza: Solar zenith angle.
+        r: Sun-Earth distance (in AU).
+
+    Returns: Reflectance.
+    """
+    return (r**2) * np.pi * rad / np.cos(sza * np.pi / 180) * f0[None, None]
+
+refl = rad_to_refl(i_stokes, f0, sza, r_sun_earth)
+
+fig, axs = plt.subplots(1, 2, figsize=(16, 8))
+axs[0].imshow(i_stokes[..., red_nadir_idx], cmap='gray')
+axs[0].set_title("Radiance")
+axs[1].imshow(refl[..., red_nadir_idx], cmap='gray')
+axs[1].set_title("Reflectance");
+
+fig, ax = plt.subplots(figsize=(16, 6))
+wv_uq = np.unique(wavelengths.values[..., 0])
+colors = ['b', 'g', 'r', 'black']
+for wv_idx in range(4):
+    wv = wv_uq[wv_idx]
+    wv_mask = wavelengths.values[..., 0] == wv
+    refl_mean = np.reshape(refl, (-1, refl.shape[-1])).mean(axis=0)
+    ax.plot(angles.values[wv_mask], refl_mean[wv_mask], '.-', label=str(wv), color=colors[wv_idx])
+ax.legend()
+ax.set_xlabel("Nominal view angle ($\\circ$)")
+ax.set_ylabel("Reflectance")
+ax.set_title("Mean reflectance by view angle");
+# -
 
 # ## HARP2: Animating the Multi-angle Data
 
 # All that is great for looking at a single angle at a time, but it doesn't capture the multi-angle nature of the instrument. Multi-angle data innately captures information about 3D structure. To get a sense of that, we'll make an animation of the scene with the 60 viewing angles available for the red band.
+#
+# WARNING: there is some mild flickering in the animation generated by the next code block.
 
 anim = APNG()
-frames = [i_stokes[..., idx] for idx in range(10, 70)]
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", message="invalid value encountered in cast", category=RuntimeWarning, module='numpy')
+    refl_int = (255 * refl).astype(np.uint8)
+frames = [refl_int[..., idx] for idx in range(10, 70)]
 frames += frames[1:][::-1]
 os.makedirs('temp_frames', exist_ok=True)
 for idx, frame in enumerate(frames):
-    frame /= frame.max()
     frame_filename = f'temp_frames/{idx:04d}.png'
-    imageio.imwrite(frame_filename, (255 * frame).astype(np.uint8))
+    imageio.imwrite(frame_filename, frame)
     anim.append_file(frame_filename, delay=idx)
 anim.save(f"images/harp2_red_anim_{harp_data.product_name.split('.')[1]}.png")
 shutil.rmtree('temp_frames');
 
+# Check it out!
+#
 # ![alt text](images/harp2_red_anim_20240421T235519.png "Title")
+
+
+
+
 
 
 
