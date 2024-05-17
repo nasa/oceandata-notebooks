9ea702799ed272e6fe40961f5b3e66cd739f46ba
Carina <carina.poulin@gmail.com>
2024-05-17 19:44:33 +0000

    New OCSSW usage notebook and update to OCSSW installation notebook

---
+++ src/oci/OCSSW_usage.py
@@ -0,0 +1,307 @@
+# # Using OCSSW functions (l2gen) using Jupyter notebooks
+#
+# **Authors:** Carina Poulin (NASA, SSAI), Ian Carroll (NASA, UMBC), Anna Windle (NASA, SSAI)
+#
+# > **PREREQUISITES**
+# >
+# > This notebook has the following prerequisites:
+# > - An **<a href="https://urs.earthdata.nasa.gov/" target="_blank">Earthdata Login</a>**
+# >   account is required to access data from the NASA Earthdata system, including NASA ocean color data.
+# > - You must have OCSSW installed on your server, see the "Installing OCSSW on a Jupyter Hub" notebook
+#
+# ## Summary
+# ***
+# [SeaDAS][seadas] is the official data analysis sofware of NASA's Ocean Biology Distributed Active Archive Center (OB. DAAC), used to process, display and analyse ocean color data. A GUI-based SeaDAS application is available, and it can be used through command line. SeaDAS's processing components found in the OCSSW package. 
+#
+# This tutorial will show you how to process PACE OCI data on a JupyterHub server using some of the OCSSW functions. 
+#
+# ## Learning Objectives
+# ***
+# <a name="toc"></a>
+# At the end of this notebok you will know:
+# * How to find and download a L1B file to your server using the earthaccess library
+# * How to process L1B data to Level 2 with l2gen
+# * How to merge two images with L2bin
+# * How to create a map with l3mapgen
+#
+# ## Contents
+# ***
+# 1. [Initial Setup](#setup)
+# 2. [Get L1B data](#data)
+# 3. [Process data with l2gen](#l2gen)
+# 4. [Merge images with L2bin](#l2bin)
+# 5. [Make a map with L3mapgen](#l3mapgen)
+#      
+# ## 1. Setup <a name="setup"></a> 
+# *** 
+# ### Imports
+#
+# We begin by importing all of the packages used in this notebook. If you have created an environment following the guidance provided with this tutorial, then the imports will be successful.
+#
+# [seadas]: https://seadas.gsfc.nasa.gov/
+
+import cartopy.crs as ccrs
+import earthaccess
+import h5netcdf
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+import pandas as pd
+import os
+
+# ## 2. Get OCI Level 1B data <a name="data"></a>
+# ***
+
+# ### Search for data
+# Set (and persist to your user profile on the host, if needed) your Earthdata Login credentials.
+
+auth = earthaccess.login(persist=True)
+
+# We will use the same search method used in OCI Data Access. Note that L1B files do not include cloud coverage metadata, so we cannot use that filter.
+
+# +
+tspan = ("2024-04-27", "2024-04-28")
+location = (-56.53125, 49.81134)
+
+results = earthaccess.search_data(
+    short_name="PACE_OCI_L1B_SCI",
+    temporal=tspan,
+    point=location,
+)
+
+quicklooks = [display(g) for g in results[0:10]]
+# -
+
+paths = earthaccess.open(results)
+
+# ### Create a directory where you will store the granules
+
+os.makedirs("granules")
+
+# ### Download the granules to the folder
+
+paths = earthaccess.download(results, "./granules")
+paths
+
+# ### Visualize L1B data 
+
+# Create a variable called paths with the L1B file
+
+path = ('granules/PACE_OCI.20240427T161654.L1B.nc')
+
+# Open the dataset using xarray
+
+dataset = xr.open_dataset(path, group="observation_data")
+dataset
+
+# We can have a look at the L1B dataset. At this level, geographical coordinates and band wavelenths are not available. We will need to process the file to Level 2 or 3 to get those.
+
+plot = dataset["rhot_red"].sel({"red_bands": 100}).plot()
+
+# ## 3. Process L1B data with l2gen <a name="l2gen"></a>
+# ***
+# L2gen will process the L1B data to L2 using the parameters you will specify for it. 
+
+# ### Run l2gen
+
+# <div class="alert alert-warning">
+# OCSSW functions need to run in the Bash language. We can create a Bash cell in a Python Jupyter Notebook using the Bash magic command (%%bash). In the specific case of OCSSW functions, we need to set up the temporary OCSSW environment in each cell where we use OCSSW functions, since they are not retained from one cell to the next. 
+# <br>    
+# <br>We need to start those cells with: <br>
+#     
+#     export OCSSWROOT=ocssw
+#     source $OCSSWROOT/OCSSW_bash.env
+#
+# </div>
+#
+# Run the l2gen command by itself to view the extensive list of options available. You can find more information about l2gen and other OCSSW functions on the [seadas website](https://seadas.gsfc.nasa.gov/help-8.3.0/processors/ProcessL2gen.html)
+
+# + scrolled=true language="bash"
+# #--- temporary environment setup for bash magic cell /
+# export OCSSWROOT=ocssw
+# source $OCSSWROOT/OCSSW_bash.env
+# #--- \
+# l2gen
+# -
+
+# To process a L1B file using L2gen, at a minimum, you need to set an infile name (ifile) and an outfile name (ofile). You can indicate a data suite, in this example, we will proceed with the surface reflectance suite used to make true color images (SFREFL).
+#
+# Feel free to explore l2gen options to produce the level 2 dataset you need. 
+
+# + scrolled=true language="bash"
+# #--- /
+# export OCSSWROOT=ocssw
+# source $OCSSWROOT/OCSSW_bash.env
+# #--- \
+#
+# l2gen ifile=/home/jovyan/ocssw_test/granules/PACE_OCI.20240427T161654.L1B.nc \
+# ofile=/home/jovyan/ocssw_test/granules/PACE_OCI.20240427T161654.L2.nc \
+# suite=SFREFL \
+# atmocor=1
+# -
+
+# Once this process is done, you are ready to visualize your L2 data!
+
+# ### Visualize L2 data
+# ***
+
+# Open your L2 netcdf file and look at the contents
+
+path = ('granules/PACE_OCI.20240427T161654.L2.nc')
+dataset = xr.open_dataset(path, group="geophysical_data")
+rhos = dataset["rhos"]
+dataset
+
+# Plot one wavelength from the dataset
+
+plot = rhos.sel({"wavelength_3d": 25}).plot(cmap="viridis")
+
+# ## 4. Merge two images with L2bin <a name="l2bin"></a>
+# ***
+# It can be useful to merge two images along a swath to create a single, larger image. L2bin is a OCSSW function that can help us do that. 
+#
+# ### Find and dowload L2 data
+# First, let's find L2 data using the earthaccess library:
+
+# +
+tspan = ("2024-04-27", "2024-04-28")
+location = (-56.95313,55.57192)
+clouds = (0, 100)
+
+results = earthaccess.search_data(
+    short_name="PACE_OCI_L2_BGC_NRT",
+    temporal=tspan,
+    point=location,
+    cloud_cover=clouds,
+)
+quicklooks = [display(g) for g in results[0:10]]
+# -
+
+# In order to download netcdf files, we need to create a directory
+
+os.makedirs("granules_l2bin")
+
+# Now let's download the granules to the directory
+
+paths = earthaccess.download(results, "./granules_l2bin")
+paths
+
+# ### L2bin processing
+# L2bin requires an infile containing the paths and names of the files to be merged. Let's create one:
+
+file_path = '/home/jovyan/ocssw_test/granules_l2bin/'
+with open("./granules_l2bin/filelist.txt", mode='w', newline='') as fp:
+    for file in os.listdir(file_path):
+        if file.endswith('.nc'):
+            f = os.path.join(file_path, file)
+            fp.write(str(f) + os.linesep) 
+
+# We can have a look at the possible options from L2bin:
+
+# + scrolled=true language="bash"
+# #--- /
+# export OCSSWROOT=ocssw
+# source $OCSSWROOT/OCSSW_bash.env
+# #--- \
+#
+# l2bin
+# -
+
+# Now run L2bin using your chosen parameters:
+
+# + scrolled=true language="bash"
+# #--- /
+# export OCSSWROOT=ocssw
+# source $OCSSWROOT/OCSSW_bash.env
+# #--- \
+#
+# l2bin ifile=/home/jovyan/ocssw_test/granules_l2bin/filelist.txt \
+# ofile=/home/jovyan/ocssw_test/granules_l2bin/PACE_OCI.20240427T161654.L3b.DAY.nc \
+# prodtype=regional \
+# resolution=1 \
+# flaguse=NONE \
+# rowgroup=2000 \
+# verbose=1
+# -
+
+# ### Visualize your L3 dataset
+# Once the L3 file is created. You can open it with xarray to visualize it. 
+
+# + scrolled=true
+dataset = xr.open_dataset('/home/jovyan/ocssw_test/granules_l2bin/PACE_OCI.20240427T161654.L3m.DAY.all.1km.nc')
+dataset
+# -
+
+# Now we can plot the merged dataset and add map elements like coastlines and gridlines. The robust parameter of the plot removes the data outliers. 
+
+# +
+fig = plt.figure()
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines(linewidth=0.5)
+ax.gridlines(draw_labels={"left": "y", "bottom": "x"}, linewidth=0.3)
+
+plot = dataset['chlor_a'].plot(x="lon", y="lat", cmap="viridis", robust=True)
+
+plt.ylim(35, 60);
+plt.xlim(-75, -50);
+# -
+
+# ## 5. Make a map with L3mapgen <a name="l3mapgen"></a>
+# ***
+
+# ### Run L3mapgen
+# The L3mapgen function of OCSSW allows you to create maps with a wide array of options you can see below:
+
+# + scrolled=true language="bash"
+# #--- /
+# export OCSSWROOT=ocssw
+# source $OCSSWROOT/OCSSW_bash.env
+# #--- \
+#
+# l3mapgen
+# -
+
+# Run L3mapgen to make a 1km map with a plate carree projection. 
+
+# + scrolled=true language="bash"
+# #--- /
+# export OCSSWROOT=ocssw
+# source $OCSSWROOT/OCSSW_bash.env
+# #--- \
+#
+# l3mapgen ifile=/home/jovyan/ocssw_test/granules_l2bin/PACE_OCI.20240427T161654.L3b.DAY.nc \
+# ofile=/home/jovyan/ocssw_test/granules_l2bin/PACE_OCI.20240427T161654.L3m.DAY.all.1km.nc \
+# projection=platecarree \
+# resolution=1km \
+# interp=bin \
+# use_quality=0 \
+# apply_pal=0
+# -
+
+# ### Visualize your map
+#
+# The level 3 map dataset can now be opened with the SeaDAS GUI application or using xarray in a jupyter notebook to produce a map. 
+#
+# Here is an example using xarray:
+
+dataset = xr.open_dataset('/home/jovyan/ocssw_test/granules_l2bin/PACE_OCI.20240427T161654.L3m.DAY.all.1km.nc')
+
+# Make a quick plot with the data:
+
+plot = dataset.chlor_a.plot(x="lon", y="lat")
+
+# Add coastines, gridlines, and remove the outliers:
+
+# +
+fig = plt.figure()
+ax = plt.axes(projection=ccrs.PlateCarree())
+ax.coastlines(linewidth=0.5)
+ax.gridlines(draw_labels={"left": "y", "bottom": "x"}, linewidth=0)
+
+plot = dataset['chlor_a'].plot(x="lon", y="lat", cmap="viridis", robust=True)
+
+plt.ylim(35, 60);
+plt.xlim(-75, -50);
+# -
+
+
---
+++ src/oci/oci_ocssw_install.py
@@ -0,0 +1,152 @@
+# # Installing OCSSW on a Jupyter Hub
+#
+# **Authors:** Carina Poulin (NASA, SSAI), Ian Carroll (NASA, UMBC), Anna Windle (NASA, SSAI)
+#
+# > **PREREQUISITES**
+# >
+# > This notebook has the following prerequisites:
+# > - An **<a href="https://urs.earthdata.nasa.gov/" target="_blank">Earthdata Login</a>**
+# >   account is required to access data from the NASA Earthdata system, including NASA ocean color data.
+# > - There are no prerequisite notebooks for this module.
+#
+# ## Summary
+# ***
+# [SeaDAS][seadas] is the official data analysis sofware of NASA's Ocean Biology Distributed Active Archive Center (OB. DAAC), used to process, display and analyse ocean color data. A GUI-based SeaDAS application is available, and it can be used through command line. SeaDAS's processing components found in the OCSSW package. 
+#
+# This tutorial will show you how to install OCSSW in a JupyterHub server and how to start doing data processing by processing a Level 1b (L1B) file from PACE OCI to a Level 2 (L2) file using L2gen.  
+#
+# ## Learning Objectives
+# ***
+# <a name="toc"></a>
+# At the end of this notebok you will know:
+# * How to install OCSSW on your server
+# * How to set up your OCSSW session
+# * How to download a L1B file to your server
+# * How to process a L1B file to L2 using l2gen
+#
+# ## Contents
+# ***
+# 1. [Initial Setup](#setup)
+# 2. [Install OCSSW](#ocssw)
+# 3. [Get L1B data](#data)
+# 4. [Process data with l2gen](#l2gen)
+#      
+# ## 1. Setup <a name="setup"></a> 
+# *** 
+#
+# <div class="alert alert-warning" role="alert"> Important! This tutorial requires a Bash kernel. 
+#     
+#     OCSSW uses the Bash language. If there is not already a Bash kernel in your JupyterHub launcher, you need to install it before doing anything *every time you launch the notebook.* Thankfully, it is easy to do with the instructions below. 
+# </div>
+#
+# ### Install a Bash Kernel
+# Run the following command. If the terminal asks you to update conda, type Y to accept.
+
+# %conda install bash_kernel
+
+# Restart the kernel (refresh button on the upper-right of the kernel window).
+#
+# ### Change your notebook kernel to Bash
+#
+# You should now change the kernel of the notebook by clicking on the kernel name in the upper-right corner of the window and selecting the Bash kernel before moving on to the rest of the tutorial. 
+#
+# [seadas]: https://seadas.gsfc.nasa.gov/
+
+# ## 2. Install OCSSW <a name="ocssw"></a>
+# ***
+#
+# <div class="alert alert-info" role="alert"> 
+# You should only need to install OCSSW on your server once. Once installed, you will see the ocssw folder in your files and it will remain there when you relaunch the notebook. 
+# </div>
+#
+# You first need to indicate the directory where you want to install ocssw:
+
+# cd /home/jovyan/ocssw
+
+# Then download the OCSSW installer to your server
+
+wget https://oceandata.sci.gsfc.nasa.gov/manifest/install_ocssw
+
+# and the manifest script
+
+wget https://oceandata.sci.gsfc.nasa.gov/manifest/manifest.py
+
+# Then you need to change permissions to the files (chmod). Take a look at the different OCSSW tags you can install. It is recommended to use the most recent one for the installation, which is T2024.16 at the time of writing this tutorial. 
+
+# + scrolled=true
+chmod +x install_ocssw
+./install_ocssw --list_tags
+# -
+
+# Install OCSSW using the tag you chose above. Choose the sensors you will be working with. In this case, we will be using OCI. A list of optional arguments can be found on the OCSSW webpage. 
+#
+# It may take some time to install all the packages. *Tip:* The process is not finished as long as the brackets on the left of the cell show [ * ]. 
+
+./install_ocssw --install_dir ocssw --tag T2024.16 --seadas --oci
+
+# <div class="alert alert-info" role="alert">
+# You have installed OCSSW! You won't need to redo this step as long as you keep the OCSSW folder on your server. 
+# </div>
+#
+
+# ## 3. Get OCI Level 1B data <a name="data"></a>
+# ***
+
+# Create a file called .netrc by **substituting LOGIN and PASSWORD** below for your own login and password before running the cell.
+
+# echo "machine urs.earthdata.nasa.gov login LOGIN password PASSWORD" > ~/.netrc ; > ~/.urs_cookies 
+chmod  0600 ~/.netrc
+
+# Download a Level 1B file using the cookies from the previous step for your earthdata credentials.
+
+# + scrolled=true
+wget --load-cookies ~/.urs_cookies \
+--save-cookies ~/.urs_cookies \
+--auth-no-challenge=on \
+--content-disposition 'https://oceandata.sci.gsfc.nasa.gov/getfile/PACE_OCI.20240427T161654.L1B.nc'
+# -
+
+# ## 4. Process L1B data with l2gen <a name="l2gen"></a>
+# ***
+# ### Set up OCSSW's l2gen
+
+# Define an environmental variable called OCSSWROOT that points to your OCSSW software installation using the export command.
+
+export OCSSWROOT=ocssw
+
+# Confirm the installation. You should see the path to OCSSSW. 
+
+printenv OCSSWROOT
+
+# Finish setting up the command line environment by sourcing the data processing environmental variable file. This file defines the locations of all files required by OCSSW. 
+
+source $OCSSWROOT/OCSSW_bash.env
+
+# Confirm the environment has been set. You should see the path to l2gen. 
+
+which l2gen
+
+# You are now ready to run l2gen, the Level-2 processing function for all ocean color instruments under the auspices of the GSFC Ocean Biology Processing Group!
+
+# ### Run l2gen
+
+# Run l2gen by itself to view the extensive list of options available. You can find more information [on the seadas website](https://seadas.gsfc.nasa.gov/help-8.3.0/processors/ProcessL2gen.html)
+
+l2gen
+
+# To process a L1B file using L2gen, at a minimum, you need to set an infile name (ifile) and an outfile name (ofile). You can indicate a data suite, in this example, we will proceed with the Surface Reflectance suite used to make true color images (SFREFL). We are turning off the atmospheric correction here to save processing time. 
+#
+# Feel free to explore l2gen options to produce the level 2 dataset you need. 
+
+# + scrolled=true
+l2gen ifile=/home/jovyan/ocssw_test/PACE_OCI.20240427T161654.L1B.nc \
+ofile=/home/jovyan/ocssw_test/PACE_OCI.20240427T161654_test.L2.nc \
+suite=SFREFL \
+atmocor=0
+# -
+
+# <div class="alert alert-info" role="alert">
+# <p>You have completed the notebook on using OCCSW to process PACE data in Jupyter notebooks. More notebooks are comming soon!</p>
+# </div>
+
+
