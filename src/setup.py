00b52b3d720bfc791f8b2485f09ed501e2ec77d2
Ian Carroll <ian.t.carroll@nasa.gov>
2024-10-04 00:06:10 -0400

    pipx run this

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = []
+# ///
+from ipykernel import kernelspec
+
+if __name__ == "__main__":
+    kernelspec.InstallIPythonKernelSpecApp().launch_instance()
