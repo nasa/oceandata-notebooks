146b5157376f7928657301943308c04f5c8dcc8f
Skye Caplan <skyelar.a.caplan@nasa.gov>
2025-03-26 16:26:31 -0400

    removed bit flag part of land tutorial

--- book/src/oci/oci_terrestrial_data.py
+++ book/src/oci/oci_terrestrial_data.py
@@ -124,66 +124,22 @@ ax.set_title('Surface reflectance at 555 nm')
 
 dataset.l2_flags
 
-# `l2_flags` is in the same shape as the surface reflectance we plotted above, but plotting the variable doesn't seem to give us any information. That's because `l2_flags` is actually a 2D array of numbers representing bitflags, so they must be treated as bits and not numbers. The meaning of each flag is described [here](https://oceancolor.gsfc.nasa.gov/resources/atbd/ocl2flags/). 
+# `l2_flags` is in the same shape as the surface reflectance we plotted above, but plotting the variable doesn't seem to give us any information. That's because `l2_flags` is actually a 2D array of numbers representing bitflags, so they must be treated as bits and not numbers.
 #
-# For example, in the surface reflectance plot above, say we want to mask clouds. Instead of trying to do some threshold math or applying your own cloud mask algorithm to each pixel, you can use the `l2_flags` variable and use L2gen's cloud mask (CLDICE) instead. Using the reference link above to figure out what bit represents clouds:
-
-bit_position = 9
-cloudmasked_dataset = dataset.where(~((dataset.l2_flags & (1 << bit_position)) != 0))
-cloudmasked_dataset
-
-# The masked dataset `cloudmasked_dataset` will retain the structure and information contained in the original merged dataset, but will take pixels where the cloud flag is flipped (i.e., where a pixel is flagged as cloudy) and assign them as NaNs. We can plot the data again to see what has changed:
-
-# +
-cloudmasked_rhos_555 = cloudmasked_dataset["rhos"].sel({"wavelength_3d": np.argwhere(wavelengths == 555)[0][0]})
-
-fig = plt.figure(figsize=(8,5))
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.coastlines(linewidth=0.5)
-ax.gridlines(draw_labels={"left": "y", "bottom": "x"},linewidth=0.25)
-cloudmasked_rhos_555.plot(x='longitude', y='latitude', cmap='viridis', vmin=0)
-ax.set_title('Surface reflectance at 555 nm with cloud mask')
-# -
-
-# To see why this works, let's break down the one line of code which performs the masking. 
+# There are a couple ways to deal with bitflags - thankfully, one of those is to use `cf_xarray`, which is a part of the `xarray` package that allows you to access any [CF metadata convention](http://cfconventions.org/) attributes present in a file. 
 #
-# Each bit in a bitmask represents a specific condition or flag (in the above example, clouds). 
-#  - The ```(1 << bit position)``` portion of the code isolates the specific "bit" in the bitmask, or in other words the specific condition you want flagged. 
-# Since clouds are at bit position 9, we set ```bit_position``` to 9 in the example above.
-#  - ```ds.l2_flags``` is the variable in our xarray dataset that contains the values for each pixel of the bitmask.
-#  - ```ds.l2_flags & (1 << bit_position)``` is basically saying to look only for pixels in the l2_flag variable where the specified bit is set. In other words, to look for pixels that have been flagged as a cloud.
-#  - ```~(ds.l2_flags & (1 << bit_position) != 0)```: The entire statement within the parentheses, with ```!= 0```, evaluates any pixel flagged cloudy as ```True```. Adding the ```~``` negates that condition, so that any pixel with the cloud flag set is evaluated to ```False```. We need these pixels to be False for the next step:
-#  - ```ds.where(~(ds.l2_flags & (1 << bit_position) != 0))```: ```xarray```'s ```.where``` function applies the mask to the entire dataset, which contains our surface reflectances. It keeps the pixel values which were evaluated to ```True``` in the step above, and assigns anything ```False``` as NaN. In other words, the cloudy pixels we set to "False" above have been set to NaN and therefore have been masked.
-#
-# We can apply multiple masks to the same datasets as well. Say you wanted to mask for clouds AND water, at the same time. There isn't a dedicated water masked, so instead we'll take the land mask at bit position 1 and invert it to mask out water. We can do that by leaving the ```~``` out of the statement, setting any non-land pixels to ```False``` like we did with clouds above:
-
-# +
-masked_dataset = dataset.where(~((dataset.l2_flags & (1 << 9)) != 0) & ((dataset.l2_flags & (1 << 1)) != 0))
-
-masked_rhos_555 = masked_dataset["rhos"].sel({"wavelength_3d": np.argwhere(wavelengths == 555)[0][0]})
-
-fig = plt.figure(figsize=(8,5))
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.coastlines(linewidth=0.5)
-ax.gridlines(draw_labels={"left": "y", "bottom": "x"},linewidth=0.25)
-masked_rhos_555.plot(x='longitude', y='latitude', cmap='viridis', vmin=0)
-ax.set_title('Surface reflectance at 555 nm with cloud & water mask')
-# -
-
-# #### Alternative method: xarray's masking capabilities
-#
-# `xarray`'s `cf_xarray` package can read flags as masks as well. This functionality has only been lightly tested, but should work for simple applications like we show below. See the [cf_xarray flag documentation](https://cf-xarray.readthedocs.io/en/latest/flags.html) here for more information.
-#
-# There are certain requirements our `xarray` dataset must meet in order to be used with `cf_xarray`. First, we need to make sure the package can read the flags properly so we can eventually apply them to the data. Let's check that the flag variable is read as such with `cf_xarray`'s built in check:
+# For example, say we want to mask any pixels flagged as clouds and or water in our data. First, we have to make sure that the `l2_flags` variable is readable by `cf_xarray` so that we can eventually apply them to the data. We can check this using the built in `is_flag_variable` function:
 
 print('Is l2_flags a flag variable?: ', dataset.l2_flags.cf.is_flag_variable)
 
-# The above statement shows the package recognizes `l2_flags`. The next step is to apply those flags, similar to the bitmask method. `Xarray` makes it easier by using the `where` function, and you don't have to understand bitwise operators to use it:
+# The statement returned "True", which means `l2_flags` is recognized as a flag variable. By referencing [this link](https://oceancolor.gsfc.nasa.gov/resources/atbd/ocl2flags/) which describes each flag, we find the name of the ones we want to mask out. In this case, "CLDICE" is the cloud flag, and while there is no specific water mask (this is an ocean mission, after all) there is a LAND flag we can invert to mask out water. The cell below will retain any pixel identified as land which is also not a cloud (thanks to the `~`):
 
-# +
 cldwater_mask = (dataset.l2_flags.cf == 'LAND') & ~(dataset.l2_flags.cf == 'CLDICE')
 land_values = dataset.where(cldwater_mask)
 
+# Then, we can see if the mask worked by plotting the same wavelength once again:
+
+# +
 land_rhos_555 = land_values["rhos"].sel({"wavelength_3d": np.argwhere(wavelengths == 555)[0][0]})
 
 fig = plt.figure(figsize=(8,6))
@@ -194,7 +150,9 @@ land_rhos_555.plot(x='longitude', y='latitude', cmap='viridis', vmin=0)
 ax.set_title('Surface reflectance at 555 nm with cloud & water mask')
 # -
 
-# Now that we have our surface reflectance data masked, we have multiple options for analysis. If your preferred data analysis environment is python, great! You can stop here. If you'd prefer to use an analysis software, specifically QGIS, you'll have to go through a couple more steps. 
+# ## 4. Working with PACE Terrestrial Data
+#
+# Now that we have our surface reflectance data masked, 
 
 # [back to top](#Contents)
 
@@ -222,8 +180,7 @@ transposed_file.to_netcdf(path=output_path)
 #
 # We're finally close to being done! If you're fine working with netCDF format, you could stop here. The final thing we'll take you through is how to convert netCDF to a GeoTIFF format.
 
-# ## 5. Veg. Indices/Classification/Working with the data
+#
 
-# +
 
 
